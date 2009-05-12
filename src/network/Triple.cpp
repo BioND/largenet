481f1a37017c33da3ff7264ccdff30e78e2d37d8
Gerd Zschaler <gerd@gzschaler.de>
2009-05-12 09:04:36 +0000

    Added PIC build flag, which is necessary for amd64 architecture.   + we should only use PIC compilation on amd64 machines   + started work on triple class

---
+++ src/network/Triple.cpp
@@ -0,0 +1,18 @@
+/*
+ * Triple.cpp
+ *
+ *  Created on: 08.05.2009
+ *      Author: gerd
+ */
+
+#include "Triple.h"
+
+namespace lnet
+{
+
+Triple::Triple(const link_id_t left, const link_id_t right) :
+	left_(left), right_(right)
+{
+}
+
+}
---
+++ src/network/Triple.h
@@ -0,0 +1,58 @@
+/*
+ * Triple.h
+ *
+ *  Created on: 08.05.2009
+ *      Author: gerd
+ */
+
+#ifndef TRIPLE_H_
+#define TRIPLE_H_
+
+#include "types.h"
+
+namespace lnet
+{
+
+class Triple
+{
+public:
+	Triple(link_id_t left, link_id_t right);
+	link_id_t left() const;
+	link_id_t right() const;
+	void setLeft(link_id_t left);
+	void setRight(link_id_t right);
+	bool containsLink(link_id_t l) const;
+
+private:
+	link_id_t left_, right_;
+};
+
+
+inline link_id_t Triple::left() const
+{
+	return left_;
+}
+
+inline link_id_t Triple::right() const
+{
+	return right_;
+}
+
+inline void Triple::setLeft(const link_id_t left)
+{
+	left_ = left;
+}
+
+inline void Triple::setRight(const link_id_t right)
+{
+	right_ = right;
+}
+
+inline bool Triple::containsLink(const link_id_t l) const
+{
+	return (l == right_) || (l == left_);
+}
+
+}
+
+#endif /* TRIPLE_H_ */
